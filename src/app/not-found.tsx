5a9c8a28b366e8df3cb97be142d7cfa4ed24d492
João Victor Carvalho <joaovictorcordeiro00@gmail.com>
2023-12-06 22:56:56 -0300

    criando página de NotFound

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return(
+        <div>
+            <h1>Não foi encontrado</h1>
+            <p>Desculpe, essa página não existe :(</p>
+        </div>
+    )
+}
